9c5d848de0a894723039a6467e96c7c4d80aaa19
chriscreilly99 <christopher.c.reilly@du.edu>
2022-05-04 19:21:52 -0600

    minor fixes to server.js

--- server.js
+++ server.js
@@ -11,12 +11,11 @@ app.use(express.static('public'));
 app.use(require('./routes'));
 
 
-// need to update route to mongodb at local host to my version 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialnetwork', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
 mongoose.set('debug', true);
 
-app.listen(PORT, () => console.log(`Running on localhost: ${PORT}`));
+app.listen(PORT, () => console.log(`Running on localhost:${PORT}`));
